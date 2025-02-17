bace92fa94e291cb08a52782a05bc45da421cf80
Pat Garcia <commonzenmedia@gmail.com>
2025-02-17 14:26:44 -0500

    Adjust location item gap

--- src/components/LocationListing/LocationListing.tsx
+++ src/components/LocationListing/LocationListing.tsx
@@ -6,7 +6,7 @@ type LocationListingProps = { locations: any };
 
 const LocationListing: FC<LocationListingProps> = ({ locations }) => {
   return (
-    <section className="px-5 py-6 flex flex-col gap-8">
+    <section className="px-5 py-6 flex flex-col gap-5">
       {locations.map((location: StorageLocation) => (
         <LocationItem key={location.id} location={location} />
       ))}
